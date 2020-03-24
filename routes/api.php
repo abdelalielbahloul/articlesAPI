a6ef8eaa4ba5f99fb3aabd4c85318001c2537a5d
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-03-24 20:30:49 +0100

    feat : add routes for articles based on jwt auth

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'ApiController@logout');
+
+    Route::resources('/articles', 'ArticleController');
 });
